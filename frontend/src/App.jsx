9cea3572fa3fc1544c1bf9eb079a6c56bd987fe3
Tushar <tusharjana.aj3@gmail.com>
2025-07-13 00:44:37 +0530

    Day2 login signup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
+import Home from './pages/Home'
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/signup' element={<SignUp />}/>
+      </Routes>
+    </>
+  )
+}
+
+export default App
